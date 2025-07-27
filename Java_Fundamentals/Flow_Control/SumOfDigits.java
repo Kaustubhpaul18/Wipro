f9e87e36c924860535033a400b3cebee972a97d9
Kaustubhpaul18 <kaustubhpaul18j@gmail.com>
2025-07-27 12:09:57 +0530

    Fix SumOfDigits logic - remove incorrect num-- statement

--- Java_Fundamentals/Flow_Control/SumOfDigits.java
+++ Java_Fundamentals/Flow_Control/SumOfDigits.java
@@ -1,10 +1,10 @@
-public class SumOfDigits{
+public class SumOfDigits {
     public static void main(String[] args) {
         int sum = 0;
-        int num=15487;
-        for(int i = num; i > 0; i--){
-            sum = sum + (i%10);
-            i = i/ 10;
+        int num = 15487;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
         }
         System.out.println("The sum of the digits is: " + sum);
     }
