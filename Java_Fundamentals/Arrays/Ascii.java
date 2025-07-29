c282c7f770194fc347c0654746caee35179bbc76
Kaustubhpaul18 <kaustubhpaul18j@gmail.com>
2025-07-29 11:27:45 +0530

    Ascii

---
+++ Java_Fundamentals/Arrays/Ascii.java
@@ -0,0 +1,8 @@
+class Ascii {
+    public static void main(String[] args) {
+        int[] asciiValues = {65, 66, 67, 68, 69};
+        for (int val : asciiValues) {
+            System.out.print((char) val + " ");
+        }
+    }
+}
