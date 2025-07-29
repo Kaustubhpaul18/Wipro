a94356f3e66c737a03414ec86ae76a6a1b28107b
Kaustubhpaul18 <kaustubhpaul18j@gmail.com>
2025-07-29 19:31:16 +0530

    Ädding all Optional Class Codes

--- OOPS-Inheritence/StringJoiner/MergeStrings.java
+++ OOPS-Inheritence/StringJoiner/MergeStrings.java
@@ -8,13 +8,11 @@ public class MergeStrings {
         StringJoiner s2 = new StringJoiner("-", "(", ")");
         s2.add("Chennai").add("Kolkata");
 
-        // Case 1: s1 merged into s2
         StringJoiner merged1 = new StringJoiner("-", "(", ")");
         merged1.merge(s1);
         merged1.merge(s2);
         System.out.println("s1 merged to s2: " + merged1);
 
-        // Case 2: s2 merged into s1
         StringJoiner merged2 = new StringJoiner("-", "[", "]");
         merged2.merge(s2);
         merged2.merge(s1);
